aa8c732c345f5ecd249b64c5ff504dc97c522e3b
sreeram <sreeram668@gmail.com>
2023-07-22 19:54:22 +0530

    pratice

---
+++ D2420230721/pratice/test.py
@@ -0,0 +1,34 @@
+val1=int(input("value1 : "))
+val2=int(input("value2 : "))
+val3=int(input("value3 : "))
+val4=int(input("value4 : "))
+val5=int(input("value5 : "))
+
+# def value(a,b,c,d,e):
+#     if operation=="add":
+#         v=(value_a+value_b+value_c)
+#         return v
+# total=(value_a)        
+# operator=(value_a,value_b,value_c)
+# div=(value_d,value_e)
+# def value(value1,value2,value3):
+#     value=(value1+value2+value3)*div
+
+#     print(value)
+#     return v
+# total=value(value1,value2,value3)
+# print(total) 
+   
+value=(val1+val2+val3)*val4*val5
+print(value)
+total=value
+if total >100:
+    print("This is a biggest number > 100")
+else:
+    print("This is a smallest number < 100")    
+
+
+
+   
+      
+
